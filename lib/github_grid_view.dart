d09a3374640e45a705ffe14f5d19e856ed865462
mr.davari <m.r.davari92@gmail.com>
2023-12-04 23:50:20 +0330

    refix widget tree

--- lib/github_grid_view.dart
+++ lib/github_grid_view.dart
@@ -33,173 +33,182 @@ class _GithubGridViewState extends State<GithubGridView> {
           margin:  const EdgeInsets.all(16),
           padding:  const EdgeInsets.all(16),
           decoration: showBorder ? BoxDecoration(border: Border.all(color: Colors.grey.withOpacity(0.8),width: 0.5,),borderRadius: const BorderRadius.only(topLeft: Radius.circular(8),topRight: Radius.circular(8))) : const BoxDecoration(),
-          child: FittedBox(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                showDate ? const Padding(
-                  padding: EdgeInsets.only(bottom: 2,left: 10,right: 88),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Text('Dec',style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500)),
-                      Text('Jan',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                      Text('Feb',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                      Text('Mar',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                      Text('Apr',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                      Text('May',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                      Text('Jun',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                      Text('Jul',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                      Text('Aug',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                      Text('Sep',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                      Text('Oct',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                      Text('Nov',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                    ],
-                  ),
-                ) : Container(),
-                Row(
+          child: Row(
+            children: [
+              showDate ? const Column(
+                children: [
+                  Text('Mon',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                  Text('Wed',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                  Text('Fri',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                ],
+              ) : const SizedBox(width: 0,)
+              ,
+              const SizedBox(width: 1,)
+              ,
+              Flexible(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    showDate ? const SizedBox(
-                      width: 30,
-                      child: Column(
+                    showDate ? const Padding(
+                      padding: EdgeInsets.only(bottom: 2,left: 10,right: 88),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisSize: MainAxisSize.max,
                         children: [
-                          Text('Mon',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                          Text('Wed',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
-                          Text('Fri',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                          Text('Dec',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                          Text('Jan',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                          Text('Feb',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                          Text('Mar',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                          Text('Apr',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                          Text('May',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                          Text('Jun',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                          Text('Jul',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                          Text('Aug',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                          Text('Sep',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                          Text('Oct',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
+                          Text('Nov',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500)),
                         ],
                       ),
-                    ) : const SizedBox(width: 0,),
-                    SizedBox(
-                      height: 350,
-                      child: GridView.builder(
-                          scrollDirection: Axis.horizontal,
-                          padding: const EdgeInsets.only(left: 24,right: 24,top: 0,bottom: 24),
-                          itemCount: grids.length,//371
-                          shrinkWrap: true,
-                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 7,
-                            crossAxisSpacing: 5,
-                            mainAxisSpacing: 5,
-                          ),
-                          itemBuilder: (ctx, index) {
-                            return GithubGridItem(
-                              index: index,
-                              themeName: themeName,
-                              initialColorNum: grids[index],
-                              onClick: (int colorNum){
-                                //print('----index : $index ---- colorNum : $colorNum');
-                                grids[index] = colorNum;
-                                //print(grids);
-                              },
-                            );
-                          }),
-                    ),
+                    ) : Container()
+                    ,
+                    FittedBox(
+                      child: SizedBox(
+                        height: 350,
+                        child: GridView.builder(
+                            scrollDirection: Axis.horizontal,
+                            padding: const EdgeInsets.only(left: 24,right: 24,top: 0,bottom: 24),
+                            itemCount: grids.length,//371
+                            shrinkWrap: true,
+                            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 7,
+                              crossAxisSpacing: 5,
+                              mainAxisSpacing: 5,
+                            ),
+                            itemBuilder: (ctx, index) {
+                              return GithubGridItem(
+                                index: index,
+                                themeName: themeName,
+                                initialColorNum: grids[index],
+                                onClick: (int colorNum){
+                                  //print('----index : $index ---- colorNum : $colorNum');
+                                  grids[index] = colorNum;
+                                  //print(grids);
+                                },
+                              );
+                            }),
+                      ),
+                    )
+                    ,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Flexible(child: Text('Github Readme Beautifier ${showAuthor ? '\'Author : M.R.Davari\'' : ''}',style: const TextStyle(color: Colors.black54),)),
+                        showProgressHint ? Row(
+                          children: [
+                            const Text('Less',style: TextStyle(color: Colors.black54)),
+                            const SizedBox(width: 6,),
+                            Container(width: 14,height: 14,decoration: BoxDecoration(color: themes.themeMap[themeName]?[0],borderRadius: BorderRadius.circular(2)),),
+                            const SizedBox(width: 5,),
+                            Container(width: 14,height: 14,decoration: BoxDecoration(color: themes.themeMap[themeName]?[1],borderRadius: BorderRadius.circular(2)),),
+                            const SizedBox(width: 5,),
+                            Container(width: 14,height: 14,decoration: BoxDecoration(color: themes.themeMap[themeName]?[2],borderRadius: BorderRadius.circular(2)),),
+                            const SizedBox(width: 5,),
+                            Container(width: 14,height: 14,decoration: BoxDecoration(color: themes.themeMap[themeName]?[3],borderRadius: BorderRadius.circular(2)),),
+                            const SizedBox(width: 5,),
+                            Container(width: 14,height: 14,decoration: BoxDecoration(color: themes.themeMap[themeName]?[4],borderRadius: BorderRadius.circular(2)),),
+                            const SizedBox(width: 6,),
+                            const Text('More',style: TextStyle(color: Colors.black54),)
+                          ],
+                        ) : Container(),
+                      ],
+                    )
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Text('Github Readme Beautifier ${showAuthor ? '\'Author : M.R.Davari\'' : ''}',style: const TextStyle(color: Colors.black54),),
-                    showProgressHint ? Row(
-                      children: [
-                        const Text('Less',style: TextStyle(color: Colors.black54)),
-                        const SizedBox(width: 6,),
-                        Container(width: 14,height: 14,decoration: BoxDecoration(color: themes.themeMap[themeName]?[0],borderRadius: BorderRadius.circular(2)),),
-                        const SizedBox(width: 5,),
-                        Container(width: 14,height: 14,decoration: BoxDecoration(color: themes.themeMap[themeName]?[1],borderRadius: BorderRadius.circular(2)),),
-                        const SizedBox(width: 5,),
-                        Container(width: 14,height: 14,decoration: BoxDecoration(color: themes.themeMap[themeName]?[2],borderRadius: BorderRadius.circular(2)),),
-                        const SizedBox(width: 5,),
-                        Container(width: 14,height: 14,decoration: BoxDecoration(color: themes.themeMap[themeName]?[3],borderRadius: BorderRadius.circular(2)),),
-                        const SizedBox(width: 5,),
-                        Container(width: 14,height: 14,decoration: BoxDecoration(color: themes.themeMap[themeName]?[4],borderRadius: BorderRadius.circular(2)),),
-                        const SizedBox(width: 6,),
-                        const Text('More',style: TextStyle(color: Colors.black54),)
-                      ],
-                    ) : Container(),
-                  ],
-                )
-              ],
-            ),
+              )
+            ],
           ),
-        ),
-        const SizedBox(height: 16,),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(width: 24,)
-            ,
-            ElevatedButton(
-                onPressed: (){
-                  grids.fillRange(0, grids.length,0);
-                  //print('-------aftch---${grids[0]}---');
-                  setState(() {
-                    //List.filled(368, 0);
-                  });
-                },
-                child: const Text('Reset')
-            )
-            ,
-            const SizedBox(width: 24,)
-            ,
-            ElevatedButton(
-                onPressed: ()async{
-                  var result = await showThemePickerDialog(themes.themeMap.keys.toList(),themeName);
-                  if(result!=null){
-                    themeName = result;
-                    //print('--------tht-- $themeName -- $result -- ');
-                    setState((){});
-                  }
-                },
-                child: const Text('Choose Theme')
-            )
-            ,
-            const SizedBox(width: 24,)
-            ,
-            ElevatedButton(
-                onPressed: (){
-                  setState(() {
-                    showBorder=!showBorder;
-                  });
-                },
-                child: Text(showBorder ? 'Hide Border' : 'Show Border')
-            )
-            ,
-            const SizedBox(width: 24,)
-            ,
-            ElevatedButton(
-                onPressed: (){
-                  setState(() {
-                    showAuthor=!showAuthor;
-                  });
-                },
-                child: Text(showAuthor ? 'Hide Author' : 'Show Author')
-            )
-            ,
-            const SizedBox(width: 24,)
-            ,
-            ElevatedButton(
-                onPressed: (){
-                  setState(() {
-                    showProgressHint=!showProgressHint;
-                  });
-                },
-                child: Text(showProgressHint ? 'Hide Progress Hint' : 'Show Progress Hint')
-            )
-            ,
-            const SizedBox(width: 24,)
-            ,
-            ElevatedButton(
-                onPressed: (){
-                  setState(() {
-                    showDate=!showDate;
-                  });
-                },
-                child: Text(showDate ? 'Hide Date' : 'Show Date')
-            )
-          ],
+        )
+        ,
+        const SizedBox(height: 16,)
+        ,
+        FittedBox(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(width: 24,)
+              ,
+              ElevatedButton(
+                  onPressed: (){
+                    grids.fillRange(0, grids.length,0);
+                    //print('-------aftch---${grids[0]}---');
+                    setState(() {
+                      //List.filled(368, 0);
+                    });
+                  },
+                  child: const Text('Reset')
+              )
+              ,
+              const SizedBox(width: 24,)
+              ,
+              ElevatedButton(
+                  onPressed: ()async{
+                    var result = await showThemePickerDialog(themes.themeMap.keys.toList(),themeName);
+                    if(result!=null){
+                      themeName = result;
+                      //print('--------tht-- $themeName -- $result -- ');
+                      setState((){});
+                    }
+                  },
+                  child: const Text('Choose Theme')
+              )
+              ,
+              const SizedBox(width: 24,)
+              ,
+              ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+                      showBorder=!showBorder;
+                    });
+                  },
+                  child: Text(showBorder ? 'Hide Border' : 'Show Border')
+              )
+              ,
+              const SizedBox(width: 24,)
+              ,
+              ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+                      showAuthor=!showAuthor;
+                    });
+                  },
+                  child: Text(showAuthor ? 'Hide Author' : 'Show Author')
+              )
+              ,
+              const SizedBox(width: 24,)
+              ,
+              ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+                      showProgressHint=!showProgressHint;
+                    });
+                  },
+                  child: Text(showProgressHint ? 'Hide Progress Hint' : 'Show Progress Hint')
+              )
+              ,
+              const SizedBox(width: 24,)
+              ,
+              ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+                      showDate=!showDate;
+                    });
+                  },
+                  child: Text(showDate ? 'Hide Date' : 'Show Date')
+              )
+            ],
+          ),
         )
       ],
     );
