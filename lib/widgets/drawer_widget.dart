b0245697041af784d41a1ca6e22941d0fb9fefaf
m.r.davari <m.r.davari92@gmail.com>
2023-12-26 20:50:52 +0330

    new build

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -26,7 +26,7 @@ class DrawerWidget extends StatelessWidget {
                 children: [
                   Stack(
                     children: [
-                      Align(alignment: Alignment.centerLeft,child: FlipperView(child: Image.asset('assets/logo_github_1.png',width: 90,height: 90,),)),
+                      Align(alignment: Alignment.centerLeft,child: FlipperView(child: Image.asset('assets/logo_github_1.png',width: 80,height: 80,),)),
                       Positioned(top: 5,right: 5,child: IconButton(onPressed: ()=>Get.back(), icon: Icon(Icons.close)))
                     ],
                   ),
@@ -61,11 +61,62 @@ class DrawerWidget extends StatelessWidget {
                     height: 12,
                   ),
                   const Text('This project developed by \'MohammadReza Davari\' using Flutter web.'),
+                  const SizedBox(height: 8,),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: InkWell(
+                      onTap: (){
+                        js.context.callMethod('open', ['mailto:m.r.davari92@gmail.com']);
+                      },
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            width: 18,height: 15,
+                              decoration: const BoxDecoration(
+                                  color: Colors.blueAccent,
+                                  borderRadius: BorderRadius.all(Radius.circular(4))
+                              ),
+                              child: const Icon(Icons.mail_outline_sharp,color: Colors.white,size: 11,)
+                          ),
+                          const SizedBox(width: 6,),
+                          const Text('m.r.davari92@gmail.com'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: InkWell(
+                      onTap: (){
+                        js.context.callMethod('open', ['https://www.linkedin.com/in/m-r-davari92']);
+                      },
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                              width: 18,height: 15,
+                              alignment: Alignment.center,
+                              padding: const EdgeInsets.only(top: 0,bottom: 3.5,left: 0,right: 0),
+                              decoration: const BoxDecoration(
+                            color: Colors.blueAccent,
+                            borderRadius: BorderRadius.all(Radius.circular(4))
+                          ),child: const Text('in',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 11),)),
+                          const SizedBox(width: 6,),
+                          const Text('linkedin.com/in/m-r-davari92',),
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(16),
+              padding: const EdgeInsets.only(top: 16,left: 16,right: 16,bottom: 8),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
@@ -90,7 +141,7 @@ class DrawerWidget extends StatelessWidget {
                   ),
                   const SizedBox(height: 8,),
                   const Text('You are welcomed to contribute, instead of copying the Idea or Code.'),
-                  const SizedBox(height: 16,),
+                  const SizedBox(height: 8,),//16
                   // const Text('How to use : ',style: TextStyle(fontWeight: FontWeight.bold),),
                   // const Padding(
                   //   padding: EdgeInsets.all(8),
@@ -114,7 +165,7 @@ class DrawerWidget extends StatelessWidget {
                           mainAxisSize: MainAxisSize.min,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            Flexible(child: Text('Please read the documents to see how to use.')),
+                            Flexible(child: Text('How to use?')),
                             SizedBox(width: 4,),
                             Icon(Icons.outbond_outlined,size: 16,color: Colors.blueAccent,)
                           ],
@@ -127,7 +178,7 @@ class DrawerWidget extends StatelessWidget {
             ),
             const Divider(indent: 10,endIndent: 10,),
             Padding(
-              padding: const EdgeInsets.all(16),
+              padding: const EdgeInsets.only(left: 16,right: 16,bottom: 16,top: 8),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
