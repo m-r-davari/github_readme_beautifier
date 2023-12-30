6ba9dc645b40a08caa75d58d52151c98eb8ff68b
m.r.davari <m.r.davari92@gmail.com>
2023-12-30 13:55:15 +0330

    fix duplicate dependencies and sharedArrayBuffer on windows

--- lib/app_bindings.dart
+++ lib/app_bindings.dart
@@ -6,21 +6,25 @@ class AppBindings extends Bindings {
 
   @override
   Future<void> dependencies() async {
-    Get.put<FFmpeg>(createFFmpeg(
+    Get.putAsync<FFmpeg>(getFFmpeg,permanent: true);
+
+  }
+
+
+
+  Future<FFmpeg> getFFmpeg ()async{
+    //await Future.delayed(const Duration(milliseconds: 2000));
+    return createFFmpeg(
       CreateFFmpegParam(
         log: true,
         corePath: "https://unpkg.com/@ffmpeg/core@0.11.0/dist/ffmpeg-core.js",
-
       ),
     )..load()..setLogger((logger) {
       if(logger.message=='ffmpeg-core loaded'){
         ConstKeeper.isFFmpegLoaded.value = true;
       }
-    }), permanent: true,);
-
+    });
   }
-
-
 
 
 
