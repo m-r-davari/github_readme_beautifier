edcb8d28d0d7eae048ccff9ed902cf1eefd09649
m.r.davari <m.r.davari92@gmail.com>
2024-01-08 19:36:20 +0330

    add typewriter sample to readme

--- lib/features/typewriter_text/presentation/pages/typewriter_text_page.dart
+++ lib/features/typewriter_text/presentation/pages/typewriter_text_page.dart
@@ -60,7 +60,9 @@ class _TypewriterTextPageState extends State<TypewriterTextPage> {
                 controller: _quillController,
                 toolbarSectionSpacing: 0,
                 sectionDividerSpace: 0,
-                fontSizesValues: const {'16':'16','20': '20', '25': '25', '30': '30','35': '35', '40': '40', '45': '45','50': '50', '55': '55', '60': '60', '65': '65', '70': '70'}
+                fontSizesValues: const {'16':'16','20': '20', '25': '25', '30': '30','35': '35', '40': '40', '45': '45',
+                  '50': '50', '55': '55', '60': '60', '65': '65', '70': '70','75': '75','80': '80','85': '85','90': '90',
+                  '95': '95','100': '100'}
               ),
             ),
           )
