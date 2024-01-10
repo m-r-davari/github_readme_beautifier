9bfd0f0a91efeb190008dab67cc8ce2d3e91cb76
m.r.davari <m.r.davari92@gmail.com>
2024-01-10 18:24:01 +0330

    adding chart

--- lib/presentation/most_used_languages/controllers/most_used_languages_controller.dart
+++ lib/presentation/most_used_languages/controllers/most_used_languages_controller.dart
@@ -3,7 +3,7 @@ import 'package:github_readme_beautifier/data/most_used_languages/repository/i_m
 
 class MostUsedLanguagesController extends GetxController {
 
-  String userName = '0x48415a484952';
+  String userName = 'm-r-davari';//0x48415a484952 //parisa3084
   final repository = Get.find<IMostLanguagesRepository>();
 
   Future<void> getMostLanguages()async{
--- lib/presentation/most_used_languages/pages/most_used_languages_pages.dart
+++ lib/presentation/most_used_languages/pages/most_used_languages_pages.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:github_readme_beautifier/presentation/most_used_languages/controllers/most_used_languages_controller.dart';
@@ -12,6 +13,14 @@ class MostUsedLanguagesPage extends StatefulWidget {
 class _MostUsedLanguagesPageState extends State<MostUsedLanguagesPage> {
 
   final controller = Get.find<MostUsedLanguagesController>();
+
+  double ipc = 1.0;
+  double ipc2 = 1.0;
+  double ipc3 = 1.0;
+  double ipc4 = 1.0;
+
+  int touchedIndex = -1;
+  bool isPieLoaded = false;
 
   @override
   void initState() {
@@ -29,14 +38,57 @@ class _MostUsedLanguagesPageState extends State<MostUsedLanguagesPage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          controller.getMostLanguages();
+          //controller.getMostLanguages();
+          setState(() {
+            isPieLoaded = true;
+            ipc = 30.0;
+            ipc2 = 40.0;
+            ipc3 = 20.0;
+            ipc4 = 10.0;
+          });
         },
         child: const Text('get'),
       ),
       body: Container(
-        child: const Center(child: Text('languages'),),
+        width: 300,height: 300,
+        child: PieChart(
+          PieChartData(
+            sections: showingSections(),
+            sectionsSpace: isPieLoaded ? 2 : 0,
+          ),
+          swapAnimationDuration: const Duration(milliseconds: 2000), // Optional
+          swapAnimationCurve: Curves.easeInCubic,
+        )
       ),
     );
   }
+
+
+
+  List<PieChartSectionData> showingSections() {
+    return List.generate(4, (i) {
+      final isTouched = i == touchedIndex;
+      final fontSize = isTouched ? 25.0 : 16.0;
+      final radius = isTouched ? 60.0 : 50.0;
+      const shadows = [Shadow(color: Colors.black, blurRadius: 2)];
+      return PieChartSectionData(
+        color: isPieLoaded
+            ? Colors.blueAccent
+            : Colors.grey,
+        value: isPieLoaded
+            ? i == 0 ? ipc : i==1 ? ipc2 : i==2? ipc3 : ipc4
+            : 100,
+        title: '',
+        radius: radius,
+        titleStyle: TextStyle(
+            fontSize: fontSize,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xffffffff)),
+      );
+    });
+  }
+
+
+
 
 }
