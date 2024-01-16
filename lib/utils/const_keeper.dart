7240a0470203c569390a98aab333e982f4052276
m.r.davari <m.r.davari92@gmail.com>
2024-01-16 20:14:46 +0330

    release most lang

--- lib/utils/const_keeper.dart
+++ lib/utils/const_keeper.dart
@@ -3,6 +3,6 @@ import 'package:get/get.dart';
 class ConstKeeper{
 
   static RxBool isFFmpegLoaded = RxBool(false);
-  static String baseUrl = 'https://api.github.com/';
+  static String baseUrl = 'https://api.github.com/';//
 
 }
