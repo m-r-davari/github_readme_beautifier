83fdf9ad6d67fee052e232f5bdee675d5a044535
m.r.davari <m.r.davari92@gmail.com>
2024-02-03 14:50:04 +0330

    release v1.0.0 -- 5 features

--- lib/utils/const_keeper.dart
+++ lib/utils/const_keeper.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 
 class ConstKeeper{
 
-  static RxBool isFFmpegLoaded = RxBool(false);//
+  static RxBool isFFmpegLoaded = RxBool(false);
   static String baseUrl = 'https://api.github.com/';
   static bool isWeb = GetPlatform.isWeb;
   static bool isDesktop = GetPlatform.isDesktop;
